dcb7d6240ce72838c1af9f7d90f473da355c48e7
felipeInv <felipedesigncont@gmail.com>
2024-01-31 19:09:40 -0300

    v1.1

--- back.js
+++ back.js
@@ -5,12 +5,15 @@ var resposta = window.document.getElementById('resposta')
 if(numero.value.length > 0){
     numero = Number(numero.value)
     if(numero == resultado){
+        resposta.style.color = "green"
        resposta.innerHTML = "Acertou!"
     }
     else if(numero > resultado){
+        resposta.style.color = "red"
         resposta.innerHTML = "Menos"
     }
-    else(numero < resultado){
+    else{
+        resposta.style.color = "red"
         resposta.innerHTML = "Mais"
     }
 }
