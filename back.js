4acd8a1b2c675d523cd6de8cf40c8b683ddff068
felipeInv <felipedesigncont@gmail.com>
2024-02-02 22:34:58 -0300

    versão v2

--- back.js
+++ back.js
@@ -2,19 +2,30 @@ var resultado = parseInt(Math.random()*1001);
 function verificar(){
 var numero = window.document.getElementById('num')
 var resposta = window.document.getElementById('resposta')
+var painelCerto = window.document.getElementById('painelCerto')
+var painelMais = window.document.getElementById('painelMais')
+var painelMenos = window.document.getElementById('painelMenos')
+var section = window.document.getElementById('section')
+painelMais.style.display = "none"
+painelCerto.style.display = "none"
+painelMenos.style.display = "none"
 if(numero.value.length > 0){
+    section.style.display = "block"
     numero = Number(numero.value)
     if(numero == resultado){
+        painelCerto.style.display = "block"
         resposta.style.color = "green"
-       resposta.innerHTML = "Acertou!"
+        resposta.innerHTML = "Acertou!"
     }
     else if(numero > resultado){
-        resposta.style.color = "red"
+        painelMenos.style.display = "block"
         resposta.innerHTML = "Menos"
+        painel.style.backgroundColor = "red"
     }
     else if(numero < resultado){
-        resposta.style.color = "red"
+        painelMais.style.display = "block"
         resposta.innerHTML = "Mais"
+        painel.style.backgroundColor = "red"
     }
 }
 }
